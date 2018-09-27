2000fea35096593fb2ead5caa2b5b356505e71f8
Roman Makarchyk <messarro@gmail.com>
2018-09-27 12:37:54 +0300

    Creating Models Chapter 7

--- students/views/groups.py
+++ students/views/groups.py
@@ -2,17 +2,32 @@
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+from ..models import Group
 
 
 def groups_list(request):
-    groups = (
-        {'id': 1,
-        'name': u'МВ-51',
-        'leader': {'id': 1, 'name': u'Джон Сендвік'}},
-        {'id': 2,
-        'name': u'МВ-52',
-        'leader': {'id': 2, 'name': u'Майкл Ліндсі'}},
-    )
+    groups = Group.objects.all()
+
+    order = request.GET.get('order_by', '')
+    if order in ('id', 'title', 'leader'):
+        groups = groups.order_by(order)
+        if request.GET.get('reverse', '') == '1':
+            groups = groups.reverse()
+
+    paginator = Paginator(groups, 3)  # Show 25 contacts per page
+
+    page = request.GET.get('page')
+    try:
+        groups = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        groups = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        contacts = paginator.page(paginator.num_pages)
+
     return render(request, 'students/groups_list.html', {'groups': groups})
 
 
--- students/views/students.py
+++ students/views/students.py
@@ -2,31 +2,52 @@
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+from ..models import Student
 
 
 def students_list(request):
-    students = (
-        {'id': 1,
-         'first_name': u'Джанґо',
-         'last_name': u'Фрімен',
-         'ticket': 235,
-         'image': 'static/img/django.jpeg'},
-        {'id': 2,
-         'first_name': u'Біллі',
-         'last_name': u'Креш',
-         'ticket': 2123,
-         'image': 'static/img/billy.jpeg'}
-    )
-    groups = (
-        {'id': 1,
-         'name': u'МВ-51',
-         'leader': {'id': 1, 'name': u'Джон Сендвік'}},
-        {'id': 2,
-         'name': u'МВ-52',
-         'leader': {'id': 2, 'name': u'Майкл Ліндсі'}},
-    )
+    students = Student.objects.all()
 
-    return render(request, 'students/students_list.html', {'students': students, 'groups': groups})
+    #students = students.order_by("last_name")
+    #students = students.order_by("last_name").reverse()
+    #students = students.order_by("ticket")
+    #students = students.order_by("ticket").reverse()
+
+
+# try to order student list
+    order = request.GET.get('order_by', '')
+    if order in ('id', 'last_name', 'first_name', 'ticket'):
+        students = students.order_by(order)
+        if request.GET.get('reverse', '') == '1':
+            students = students.reverse()
+
+
+# paginate students
+    paginator = Paginator(students, 3)  # Show 3 students per page
+    # ми отримуємо номер поточної сторінки, для якої ми маємо зібрати(?) студентів. По замовчуванню це буде перша.
+    page = request.GET.get('page')
+    try:
+        # students тут тепер це не простий список QuerySet, а це тепер спеціальний wrapper, педжінейтор пейдж, який має цілий набір методів.
+        students = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        students = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        students = paginator.page(paginator.num_pages)
+
+    # groups = (
+    #     {'id': 1,
+    #      'name': u'МВ-51',
+    #      'leader': {'id': 1, 'name': u'Джон Сендвік'}},
+    #     {'id': 2,
+    #      'name': u'МВ-52',
+    #      'leader': {'id': 2, 'name': u'Майкл Ліндсі'}},
+    # )
+
+    return render(request, 'students/students_list.html', {'students': students})
 
 
 def students_add(request):
